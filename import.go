f83c143f0ee30b1a82c7e6d3a1bc8af61355ec9a
felixreverett <felixreverett@gmail.com>
2025-03-27 11:09:28 +0000

    ✨ added json unmarshalling function for secrets.json

--- import.go
+++ import.go
@@ -1,3 +1,22 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 // Place for code to import settings data
+func LoadSecrets(filename string) (*Secrets, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read secrets file: %v\nDoes the file exist on your local machine?", err)
+	}
+
+	var secrets Secrets
+	if err := json.Unmarshal(data, &secrets); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON: %v", err)
+	}
+
+	return &secrets, nil
+}
