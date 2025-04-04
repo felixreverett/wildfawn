0f46f66199838cb911312a0fdc40a07c79e04c0b
felixreverett <felixreverett@gmail.com>
2025-04-04 16:20:33 +0100

    ✨ added IsSiteDue() receiver function & SiteCrawlConfig struct

--- import.go
+++ import.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"time"
 )
 
 type Secrets struct {
@@ -23,4 +24,42 @@ func LoadSecrets(filename string) (*Secrets, error) {
 	}
 
 	return &secrets, nil
+}
+
+type SiteCrawlConfig struct {
+	Root           string
+	FirstAdded     string
+	CrawlFrequency string
+	SheetName      string
+	SheetID        string
+}
+
+func ImportSiteCrawlInfo() /*[]SiteCrawlConfig*/ {
+	// Import Site Crawl Configurations from dedicated Google Sheet (or appropriate source)
+}
+
+func (s SiteCrawlConfig) IsSiteDue() (bool, error) {
+	startDate, err := time.Parse("2006-01-02", s.FirstAdded)
+	if err != nil {
+		return false, fmt.Errorf("invalid date format: %v", err)
+	}
+
+	var duration time.Duration
+
+	switch s.CrawlFrequency {
+	case "daily":
+		duration = 24 * time.Hour
+	case "weekly":
+		duration = 7 * 24 * time.Hour
+	case "fortnightly":
+		duration = 14 * 24 * time.Hour
+	case "monthly":
+		duration = 28 * 24 * time.Hour
+	default:
+		return false, fmt.Errorf("invalid crawl frequency: '%s'. Expected one of: daily, weekly, fortnightly, monthly", s.CrawlFrequency)
+	}
+
+	targetDate := startDate.Add(duration)
+
+	return time.Now().After(targetDate), nil
 }
