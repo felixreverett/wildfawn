4d1063e7a7c73454eba872548c0c7f8581de53c6
Felix Everett <felixreverett@gmail.com>
2025-04-14 07:36:28 +0100

    🧹 tweaked method signatures to return values rather than pointers; improved error logging

--- export.go
+++ export.go
@@ -22,20 +22,21 @@ import (
 
 //func WriteNewSheet(sheetID string, data map[string]*URLObject) error {}
 
-func WriteToSheet(sheetID string, sheetName string, data map[string]*URLObject) error {
+func WriteToSheet(sheetID string, sheetName string, URLObjectList URLObjectList) error {
+	data := URLObjectList.URLObjects
 	start := time.Now()
-	fmt.Printf("Writing export to sheets\n")
+	fmt.Printf(">   Writing export to sheets\n")
 
 	// 1. Load credentials
 	credentials, err := os.ReadFile("service_account.json")
 	if err != nil {
-		return fmt.Errorf("error: failed to read credentials file: %v", err)
+		return fmt.Errorf("[!] Error: failed to read credentials file: %v", err)
 	}
 
 	// 2. Configure JSON web token to authenticate requests to the Google Sheets API
 	config, err := google.JWTConfigFromJSON(credentials, sheets.SpreadsheetsScope)
 	if err != nil {
-		return fmt.Errorf("error: failed to parse credentials: %v", err)
+		return fmt.Errorf("[!] Error: failed to parse credentials: %v", err)
 	}
 
 	client := config.Client(context.Background())
@@ -43,15 +44,17 @@ func WriteToSheet(sheetID string, sheetName string, data map[string]*URLObject) 
 	// Creating Google Sheets service
 	service, err := sheets.NewService(context.Background(), option.WithHTTPClient(client))
 	if err != nil {
-		return fmt.Errorf("error: failed to create Sheets service: %v", err)
+		return fmt.Errorf("[!] Error: failed to create Sheets service: %v", err)
 	}
 
 	// Convert URLObject to interface for Sheets
 	var values [][]interface{}
-	values = append(values, []interface{}{"URL", "Inlinks", "Outlinks", "Page Status", "Crawl Depth", "Indexability", "Canonical"}) //headers
+	values = append(values, []interface{}{
+		"URL", "Inlinks", "Outlinks", "Page Status", "Crawl Depth", "No Index", "Indexability", "Canonical", "Self-Canonicalises", "Is Canonical Indexable", "Is Orphan"}) //headers
 
 	for url, obj := range data {
-		row := []interface{}{url, obj.Inlinks, obj.Outlinks, obj.PageStatus, obj.CrawlDepth, obj.Indexability, obj.Canonical}
+		row := []interface{}{
+			url, obj.Inlinks, obj.Outlinks, obj.PageStatus, obj.CrawlDepth, obj.NoIndex, obj.Indexability, obj.Canonical, obj.IsSelfCanonicalising, obj.IsCanonicalIndexable, obj.IsOrphan}
 		values = append(values, row)
 	}
 
@@ -67,23 +70,23 @@ func WriteToSheet(sheetID string, sheetName string, data map[string]*URLObject) 
 	_, err = service.Spreadsheets.Values.Update(sheetID, writeRange, valueRange).ValueInputOption("RAW").Do()
 
 	if err != nil {
-		return fmt.Errorf("error: failed to write data to sheet: %v", err)
+		return fmt.Errorf("[!] Error: failed to write data to sheet: %v", err)
 	}
 
-	fmt.Printf("Data successfully written to Sheet in %s\n", time.Since(start))
+	fmt.Printf(">   Data successfully written to Sheet in %s\n", time.Since(start))
 
 	return nil
 }
 
-func WriteWild(URLObjects map[string]*URLObject) {
+func WriteWild(URLObjectList URLObjectList) {
 
 	secrets, err := LoadSecrets("secrets.json")
 	if err != nil {
-		fmt.Println("Error loading secrets:", err)
+		fmt.Println("[!] Error loading secrets:", err)
 		return
 	}
 
-	if err := WriteToSheet(secrets.SheetID, secrets.SheetName, URLObjects); err != nil {
-		fmt.Printf("Error: %v", err)
+	if err := WriteToSheet(secrets.SheetID, secrets.SheetName, URLObjectList); err != nil {
+		fmt.Printf("[!] Error writing to sheet: %v", err)
 	}
 }
