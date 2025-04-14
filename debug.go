3cb9c3e9bd1c757b25652d5e4fb0dc65fde56825
Felix Everett <felixreverett@gmail.com>
2025-04-14 07:36:01 +0100

    🧹 commented out GoTame() as function is temporarily deprecated

--- debug.go
+++ debug.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func GoTame() {
 	// substitutes a crawl (so I don't recrawl every time)
-	URLObjects := make(map[string]*URLObject)
+	/*URLObjects := make(map[string]*URLObject)
 	URLObjects["https://example.com/page1"] = &URLObject{Inlinks: 5, Outlinks: 3, PageStatus: 200, CrawlDepth: 2}
 	URLObjects["https://example.com/page2"] = &URLObject{Inlinks: 8, Outlinks: 2, PageStatus: 301, CrawlDepth: 3}
 	URLObjects["https://example.com/page3"] = &URLObject{Inlinks: 2, Outlinks: 7, PageStatus: 404, CrawlDepth: 1}
@@ -21,18 +21,18 @@ func GoTame() {
 
 	if err := WriteToSheet(secrets.SheetID, secrets.SheetName, URLObjects); err != nil {
 		fmt.Printf("Error: %v", err)
-	}
+	}*/
 }
 
 func PrintSiteMap(robots Robots) {
-	fmt.Println("Debug - logging robots file:")
-	fmt.Println("Agents:")
+	fmt.Println("(i) Debug - logging robots file:")
+	fmt.Println(">   Agents:")
 	for _, agent := range robots.Agents {
-		fmt.Printf("Agent name: %s\nAgent disallows: %v\nAgent allows: %v\n", agent.Name, agent.Disallow, agent.Allow)
+		fmt.Printf(">   Agent name: %s\n>   Agent disallows: %v\n>   Agent allows: %v\n", agent.Name, agent.Disallow, agent.Allow)
 	}
-	fmt.Println("Sitemaps:")
+	fmt.Println(">   Sitemaps:")
 	for _, sitemap := range robots.Sitemaps {
-		fmt.Println(sitemap)
+		fmt.Println(">   ", sitemap)
 	}
-	fmt.Printf("CrawlDelay: %d\n", robots.CrawlDelay)
+	fmt.Printf(">   CrawlDelay: %d\n", robots.CrawlDelay)
 }
