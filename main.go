306fbba329003ab9e160de7a8d4d7c0508fa9c2e
Felix Everett <felixreverett@gmail.com>
2025-04-14 07:34:04 +0100

    🧹 improved error logging; general cleanliness tweaks

--- main.go
+++ main.go
@@ -9,14 +9,18 @@ func main() {
 	// temporary bypass of imports
 	data, err := os.ReadFile("rooturl.txt")
 	if err != nil {
-		fmt.Println("Error loading File: ", err)
+		fmt.Println("[!] Error loading file. Aborting crawl: ", err)
+		return
 	}
 	url := string(data)
 
 	// 2. Crawl
-	URLObjects := GoWild(url)
+	URLObjectList, err := GoWild(url)
+	if err != nil {
+		fmt.Println("[!] Error crawling root URL. Aborting crawl. Error: ", err)
+		return
+	}
 
 	// 3. Export
-	WriteWild(URLObjects)
-	//GoTame() //test method which bypasses a crawl and exports data
+	WriteWild(URLObjectList)
 }
