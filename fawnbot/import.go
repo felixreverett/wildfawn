45babb67afcfba4dac078090c16041e3d3facc90
Felix Everett <felixreverett@gmail.com>
2025-04-21 08:12:05 +0100

    🧹 removed secrets (deprecated)

--- fawnbot/import.go
+++ fawnbot/import.go
@@ -37,27 +37,6 @@ func LoadProgramConfig(filename string) (ProgramConfig, error) {
 
 // - - -
 
-type Secrets struct {
-	SheetID   string `json:"SheetID"`
-	SheetName string `json:"SheetName"`
-}
-
-func LoadSecrets(filename string) (*Secrets, error) {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read secrets file: %v\nDoes the file exist on your local machine?", err)
-	}
-
-	var secrets Secrets
-	if err := json.Unmarshal(data, &secrets); err != nil {
-		return nil, fmt.Errorf("failed to parse JSON: %v", err)
-	}
-
-	return &secrets, nil
-}
-
-// - - -
-
 type CrawlConfig struct {
 	Root           string `json:"Root"`
 	CrawlStart     string `json:"CrawlStart"`
